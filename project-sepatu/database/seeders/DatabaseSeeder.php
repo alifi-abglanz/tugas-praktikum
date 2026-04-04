015ac89b24406fe24ab15bca460cc6bb8b440196
alifi-abglanz <alifi.abglanz@mhs.itenas.ac.id>
2026-04-05 06:27:46 +0700

    restore

--- project-sepatu/database/seeders/DatabaseSeeder.php
+++ project-sepatu/database/seeders/DatabaseSeeder.php
@@ -5,9 +5,6 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Carbon;
-use Illuminate\Support\Facades\DB;
-
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,37 +14,12 @@ class DatabaseSeeder extends Seeder
      * Seed the application's database.
      */
     public function run(): void
-        {
-            
+    {
+        // User::factory(10)->create();
 
-
-       DB::table('categories')->insert([
-        [
-            'category_id' => 1,
-            'category_name' => 'Sneakers',
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now(),
-        ],
-        [
-            'category_id' => 2,
-            'category_name' => 'Sports',
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now(),
-        ]
-    ]);
-
-    DB::table('products')->insert([
-        [
-            'product_id' => 1,
-            'category_id' => 1,
-            'product_name' => 'Nike Air Force 1',
-            'product_price' => 100000,
-            'product_stock' => 10,
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now(),
-        ]
-    ]);
-
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
     }
-    
 }
